6caa01a241f32e16c1ed948ea20d71481f388099
HYEONJI LEE <55472485+hyeonzii@users.noreply.github.com>
2022-11-23 17:06:09 +0900

    userpage

---
+++ db-frontend/src/component/user-my-page/MyBalance.jsx
@@ -0,0 +1,109 @@
+import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import styled from "styled-components";
+import BalTab from "./tab/BalTab";
+import ChargeTab from "./tab/ChargeTab";
+
+
+function MyBalance () {
+
+    const [selTab,setSelTab] = useState(0);
+    const [tabName,setTabName] =useState('잔액');
+
+    const obj = {
+        0: <BalTab />,
+        1: <ChargeTab />
+    }
+
+    const ChargeBtn = () => {
+        if(selTab === 0){
+            setSelTab(1);
+            setTabName('충전');
+        }
+        else{
+            setSelTab(0);
+            setTabName('잔액');
+        }
+        return;
+    }
+
+
+
+    return(
+<div className="balancepage">
+<StyledLayout>
+    <Box>
+        <Title>예수금</Title><RightLayout><StyledBtn className={selTab ? 'balance' : 'charge'} onClick={ChargeBtn}>{tabName}</StyledBtn></RightLayout>
+    </Box>
+    <Box>
+        <GrayLayout>
+            {obj[selTab]}
+        </GrayLayout>
+    </Box>
+</StyledLayout>
+</div>
+    );
+}
+
+export default MyBalance;
+
+//스타일 레이아웃
+const StyledLayout = styled.div`
+border-radius: 50px 50px 50px 50px;
+box-shadow: ${(props) => props.theme.defaultShadow};
+padding:10px;
+
+`;
+
+//기업명 기업정보 아이콘 묶는 레이아웃
+const Box =styled.div`
+display:flex;
+padding:10px;
+margin:10px;
+`;
+
+//오른 정렬 레이아웃
+const RightLayout = styled.div`
+  width:200px;
+  height:40px;
+  text-align:right;
+   margin-left:auto;
+`;
+
+//제목
+const Title = styled.div`
+  font-size:25px;
+  margin-top:3px;
+  margin-left:5px;
+  font-weight:bold;
+`;
+
+//충전/잔액 탭 버튼
+const StyledBtn = styled.button`
+  font-size:15px;
+  text-align:center;
+  border-radius:30px;
+  width:30%;
+  border:none;
+  padding:7px;
+  ${(props)=> {
+    if(props.className === 'charge'){
+        return `background-color: rgb(217,217,217);
+        `;
+    }
+    else if(props.className === 'balance'){
+        return `background-color: black;
+        color:white;
+        `;
+    }
+  }};
+`;
+
+//회색 레이아웃
+const GrayLayout =styled.div`
+width:100%;
+height:50px;
+border-radius: 15px;
+background-color:rgb(217,217,217);
+padding:15px;
+`;
---
+++ db-frontend/src/component/user-my-page/MyStock.jsx
@@ -0,0 +1,123 @@
+import styled from "styled-components";
+
+
+function MyStock () {
+    
+    return(
+<div className="myStockPage">
+    <StyledLayout>
+        <Box>
+            <Title> 보유주식</Title>
+        </Box>
+        <CateBox>
+            <LittleTitle>종목명</LittleTitle>
+            <LittleTitle style={{marginLeft:'30px'}}>보유수량</LittleTitle>
+            <RightLayout>
+                <div style={{display:'flex'}}>
+                <LittleTitle>매입가</LittleTitle>
+                <LittleTitle style={{marginLeft:'25px'}}>현재가</LittleTitle>
+                <LittleTitle style={{marginLeft:'25px'}}>평가손익</LittleTitle>
+                </div>
+            </RightLayout>
+        </CateBox>
+        <ScrBox>
+            <GrayMargin>
+            <GrayLayout>
+                <LittleTitle>기업1</LittleTitle>
+                <LittleTitle style={{marginLeft:'30px'}}>10</LittleTitle>
+            <RightLayout >
+            <div style={{display:'flex'}}>
+                <LittleTitle>50000원</LittleTitle>
+                <LittleTitle style={{marginLeft:'25px'}}>500000원</LittleTitle>
+                <LittleTitle style={{marginLeft:'25px'}}>50000원</LittleTitle>
+                </div>
+            </RightLayout>
+            </GrayLayout>
+            </GrayMargin>
+        </ScrBox>
+        <Box>
+            <MiddleTitle>총 수익률</MiddleTitle><RightLayout><Title>5.0%</Title></RightLayout>
+        </Box>
+        <Box>
+            <MiddleTitle>총 평가 금액</MiddleTitle><RightLayout><Title>130000원</Title></RightLayout>
+        </Box>
+    </StyledLayout>
+</div>
+    );
+}
+
+export default MyStock;
+
+//회색 박스 간격
+const GrayMargin = styled.div`
+display:flex;
+margin-left:10px;
+margin-right:10px;
+margin-bottom:10px;
+`;
+
+//스타일 레이아웃
+const StyledLayout = styled.div`
+border-radius: 50px 50px 50px 50px;
+box-shadow: ${(props) => props.theme.defaultShadow};
+padding:10px;
+margin-top:30px;
+`;
+
+//기업명 기업정보 아이콘 묶는 레이아웃
+const Box =styled.div`
+display:flex;
+padding:10px;
+margin:10px;
+`;
+
+const CateBox = styled.div`
+display:flex;
+margin-left:20px;
+margin-right:20px;
+`;
+
+//스크롤 페이지
+const ScrBox = styled.div`
+overflow:auto;
+height:200px;
+`;
+
+//오른 정렬 레이아웃
+const RightLayout = styled.div`
+  width:50%;
+  height:40px;
+  text-align:right;
+   margin-left:auto;
+`;
+
+//제목
+const Title = styled.div`
+  font-size:25px;
+  margin-top:3px;
+  margin-left:5px;
+  font-weight:bold;
+`;
+
+const MiddleTitle = styled.div`
+font-size:20px;
+margin-top:3px;
+margin-left:5px;
+`;
+
+//소제목
+const LittleTitle = styled.div`
+font-size:15px;
+margin-top:3px;
+margin-left:5px;
+`;
+
+//회색 레이아웃
+const GrayLayout =styled.div`
+width:100%;
+height:50px;
+border-radius: 15px;
+background-color:rgb(217,217,217);
+padding:15px;
+display:flex;
+`;
